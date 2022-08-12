a207453f8b96016e0d3011fe032ad068323534b5
Juboskar <oskari.juvakka@gmail.com>
2022-08-12 12:16:50 +0300

    set default port

--- city-bikes-backend/src/config.ts
+++ city-bikes-backend/src/config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT as string;
+export const PORT = process.env.PORT ? process.env.PORT : 3001;
 export const MONGODB_URI = process.env.MONGODB_URI as string;
