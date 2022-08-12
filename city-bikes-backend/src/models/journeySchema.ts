a5b3877ff3b8ede2eae3a4805aa06dd83dbaafb9
Juboskar <oskari.juvakka@gmail.com>
2022-08-12 19:16:46 +0300

    update schema

--- city-bikes-backend/src/models/journeySchema.ts
+++ city-bikes-backend/src/models/journeySchema.ts
@@ -5,7 +5,9 @@ const journeySchema = new mongoose.Schema<Journey>({
   departure: { type: Date, required: true },
   return: { type: Date, required: true },
   departure_station_id: { type: Number, required: true },
+  departure_station_name: { type: String, required: true },
   return_station_id: { type: Number, required: true },
+  return_station_name: { type: String, required: true },
   distance: { type: Number, required: true },
   duration: { type: Number, required: true }
 });
--- city-bikes-backend/src/types.ts
+++ city-bikes-backend/src/types.ts
@@ -2,7 +2,9 @@ export interface Journey {
   departure: Date;
   return: Date;
   departure_station_id: number;
+  departure_station_name: string;
   return_station_id: number;
+  return_station_name: string;
   distance: number;
   duration: number;
 }
